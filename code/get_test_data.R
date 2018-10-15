5e869584c5c47ab3b3641f9301983c4ffba17aa7
akhagan <akhagan@umich.edu>
2018-10-15 16:33:23 -0400

    redo with new datasets

--- code/get_test_data.R
+++ code/get_test_data.R
@@ -1,21 +1,37 @@
 library(tidyverse)
-library(stringr)
 
-manu_data <- read_csv("processed_data/all_manu_all_xml.csv") %>% 
+manu_data <- read_csv("processed_data/2018_grouped_manu_fixed.csv") %>% 
   mutate(doi = tolower(doi))
 
-people_data <- read_csv("processed_data/all_people_all_xml.csv") %>% 
+manu_data_clean <- manu_data %>% select(-related.manu, -is.resubmission)
+
+people_data <- read_csv("processed_data/2018_people.csv") %>% 
   filter(role == "editor"|role == "senior.editor") %>% 
-  select(person.id, manuscript.number, first.name, role) %>% 
+  select(person.id, manuscript.number, contains("name"), role) %>% 
   distinct()
 
-review_data <- read_csv("processed_data/all_reviews_all_xml.csv")
+review_data <- read_csv("processed_data/2018_reviews.csv")
 
-published_data <- read_csv("processed_data/all_published.csv")
+usage_files <- list.files("processed_data/usage", full.names = TRUE)
 
-report_data <- left_join(manu_data, people_data, by = "manuscript.number") %>% 
+usage_data <- map_df(usage_files, read_csv)
+
+usage_select <- usage_data %>% select(`Article Date of Publication (article_metadata)`, `Article DOI (article_metadata)`, `Measure Names`,
+                                      `Measure Values`, `Published Months`)
+
+citation_files <- list.files("processed_data/citations", full.names = TRUE)
+
+citation_data <- map_df(citation_files, read_csv)
+
+citation_select <- citation_data %>% select(`Article DOI (article_metadata)`, `Date of Publication`, `Measure Names`, `Measure Values`, `Published Months`)
+
+published_data <- full_join(citation_select, usage_select, by = c("Date of Publication" = "Article Date of Publication (article_metadata)", "Published Months", "Measure Names", "Measure Values", "Article DOI (article_metadata)"))
+
+report_data <- left_join(manu_data_clean, people_data, by = "manuscript.number") %>% 
   left_join(., review_data, by = "manuscript.number") %>% 
   left_join(., published_data, by = c("doi" = "Article DOI (article_metadata)"))
+
+report_data <- rename(report_data, "editor.id" = "person.id.x", "reviewer.id" = "person.id.y")
 
 write_csv(report_data, "processed_data/test_report_data.csv")
 
