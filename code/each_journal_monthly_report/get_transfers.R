75514a3d80477cf8e57225cfa422d804a0594c5a
akhagan <akhagan@umich.edu>
2019-05-24 16:45:26 -0400

    drop caps on editor & ejp colnames

--- code/each_journal_monthly_report/get_transfers.R
+++ code/each_journal_monthly_report/get_transfers.R
@@ -2,7 +2,7 @@
 
 J_transfer_data <- data %>% 
   filter(journal == this_journal) %>% 
-  filter(!is.na(transfer.journal) & transfer.type == "From") %>% 
+  filter(!is.na(transfer.journal) & transfer.type == "From") %>%
   filter(role == "editor") %>% 
   rename(transfer.from = transfer.journal) %>% 
   mutate(transfer.date = date(transfer.date))
@@ -10,11 +10,11 @@ J_transfer_data <- data %>%
 #1. how many transfers to each journal in the previous month
 J_recent_transfers <- J_transfer_data %>% 
   filter(transfer.date %within% interval(start = (today() - days(30)), end = today())) %>% 
-  select(transfer.date, transfer.from, manuscript.number, title, Editor, EJP.decision)
+  select(transfer.date, transfer.from, manuscript.number, title, editor, ejp.decision)
   
 #2. what percentage of transfers accepted in the previous year
 J_transfers_ytd <- J_transfer_data %>% 
   filter_12_mo(., .$transfer.date) %>% 
-  filter(EJP.decision == "Reject" | EJP.decision == "Accept, no revision") %>% 
-  group_by(EJP.decision) %>% summarise(n = n()) %>% 
+  filter(ejp.decision == "Reject" | ejp.decision == "Accept, no revision") %>% 
+  group_by(ejp.decision) %>% summarise(n = n()) %>% 
   mutate(prop = get_percent(n, sum(n)))
