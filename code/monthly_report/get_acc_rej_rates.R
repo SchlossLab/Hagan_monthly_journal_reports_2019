707e0139058492a475d7fc4f5c6e207689aefc7f
akhagan <akhagan@umich.edu>
2019-06-03 16:14:08 -0400

    convert na's to zeros

--- code/monthly_report/get_acc_rej_rates.R
+++ code/monthly_report/get_acc_rej_rates.R
@@ -13,7 +13,7 @@ acc_rej_data <- data %>%
 all_desc <- acc_rej_data %>% group_by(journal) %>% 
   summarise(total = n()) %>% as.data.frame() #count rej/acc decisions by journal
 
-total_row <- tibble(journal = "All", total = sum(all_desc[1:15,2])) #generate total row
+total_row <- tibble(journal = "All", total = sum(all_desc[1:dim(all_desc)[1],2])) #generate total row
 
 all_desc <- all_desc %>% rbind(total_row) # bind total decisions to journal
 
@@ -24,7 +24,7 @@ ed_reject <- acc_rej_data %>% filter(ejp.decision == "Reject" & is.na(version.re
 
 ed_rej_row <- tibble(journal = "All", editorial_rejections = sum(ed_reject[1:nrow(ed_reject),2])) #generate total row for ed rej
 
-ed_reject <- ed_reject %>% rbind(ed_rej_row) %>% test_list(all_desc, ., "journal") %>% #bind total ed rej row
+ed_reject <- ed_reject %>% rbind(ed_rej_row) %>% test_list(all_desc, ., "journal")  %>% #bind total ed rej row
   mutate(percent_ed_rej = get_percent(editorial_rejections, all_desc$total)) #add column counting percents
 
 #calculate rejection rates----
@@ -33,16 +33,16 @@ reject <- acc_rej_data %>% filter(ejp.decision == "Reject" & !is.na(version.revi
 
 rej_row <- tibble(journal = "All", rejected = sum(reject[1:nrow(reject),2])) #generate total row for rejections
 
-reject <- reject %>% rbind(rej_row) %>% test_list(all_desc, ., "journal") %>% #add total row
+reject <- reject %>% rbind(rej_row) %>% test_list(all_desc, ., "journal") %>%  #add total row
   mutate(percent_rej = get_percent(rejected, all_desc$total)) #add column counting percents
 
 #calculate acceptance rates----
 accept <- acc_rej_data %>% filter(ejp.decision == "Accept, no revision") %>% #accepts
   group_by(journal) %>% summarise(accepted = n()) %>% as.data.frame() #count by journals & convert to df
 
-accept_row <- tibble(journal = "All", accepted = sum(accept[1:15,2])) #generate row for total accepts
+accept_row <- tibble(journal = "All", accepted = sum(accept[1:dim(accept)[1],2])) #generate row for total accepts
 
-accept <- accept %>% rbind(accept_row) %>% test_list(all_desc, ., "journal") %>% #add total accepts row
+accept <- accept %>% rbind(accept_row) %>% test_list(all_desc, ., "journal")  %>% #add total accepts row
   mutate(percent_accepted = get_percent(accepted, all_desc$total)) #add column counting percents
 
 #summary df-----
