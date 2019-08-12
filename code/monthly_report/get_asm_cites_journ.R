d429f82ea18ed1c7ef4c74233cc2eba9c01c0ef9
akhagan <akhagan@umich.edu>
2019-08-12 17:24:05 -0400

    clean & comment

--- code/monthly_report/get_asm_cites_journ.R
+++ code/monthly_report/get_asm_cites_journ.R
@@ -2,9 +2,10 @@
 #Data needed: doi, WoS citation stats, manuscript type
 
 cites_data <- data %>% 
-  filter_12_to_36_mo(., .$publication.date) %>% 
+  filter_12_to_36_mo(., .$publication.date) %>% #articles published between 1 and 3 years previous
   filter(measure.names == "Total Article Cites")
 
+#citations by journal----
 journal_cites_plot <- cites_data %>% 
   ggplot()+
   geom_boxplot(aes(x = journal, y = measure.values))+
@@ -12,72 +13,9 @@ journal_cites_plot <- cites_data %>%
        subtitle = "Articles published in the previous 12 to 36 months")+
   my_theme
 
-#categories <- category_cites_data %>% group_by(category) %>% summarise(n = n())
-
+#citations according to the manuscript category----
 category_cites_data <- cites_data %>% 
-  mutate(category = fct_collapse(category,
-    "Antimicrobials" = c("Antibacterial and antifungal agents",
-                         "Antiviral agents", "Antiviral Agents",
-                         "Pharmacology", "Experimental Therapeutics", 
-                         "Biologic Response Modifiers"),
-    "Clinical" = c("Clinical Immunology and Vaccines",
-                   "Clinical Microbiology Best Practices",
-                   "Clinical Therapeutics",
-                   "Clinical Veterinary Microbiology", 
-                   "Fungal and Parasitic Infections",
-                   "Bacterial Infections",
-                   "Analytical Procedures",
-                   "Practical Guidance for Clinical Microbiology (formerly &lt;i&gt;Cumitechs&lt;/i&gt;)"),
-    "Virology" = c("Virology", "Virus-Cell Interactions", "Viruses",
-                   "(-) strand RNA viruses, double-strand RNA viruses",
-                   "(+) strand RNA viruses, including retroviruses/HIV",
-                   "Structure and Assembly", "DNA viruses",
-                   "Transformation and Oncogenesis"),
-    "Mycology" = c("Fungi and yeast", "Mycology",
-                   "Mycobacteriology and Aerobic Actinomycetes"),
-    "Parasitology" = c("Parasites", "Parasitology"),
-    "Pathogenesis & Immunity" = c("Pathogenesis", 
-                                  "Pathogenesis and Immunity", 
-                                  "Molecular Pathogenesis", 
-                                  "Cellular Microbiology: Pathogen-Host Cell Molecular Interactions", 
-                                  "Host Response and Inflammation",
-                                  "Cellular Response to Infection",
-                                  "Susceptibility"),
-    "Vaccine Immunology" = c("Microbial Immunity and Vaccines", 
-                             "Vaccines and Antiviral Agents"),
-    "Public Health" = c("Epidemiology", "Epidemiology and Surveillance",
-                       "Public and Environmental Health Microbiology",
-                       "Food Microbiology"),
-    "Omics" = c("Metagenomics and bioinformatics", "Molecular Genomics",
-                "Microbial Genomics"),
-    "Host Microbiomes" = c("Host-Associated Microbial Communities", 
-                           "Host-Microbiome Relationships", 
-                           "Invertebrate Microbiology", "Microbiome"),
-    "Physiology" = c("Microbial Metabolism", 
-                     "Mechanisms of Action: Physiological Effects"),
-    "Ecology" = c("Microbial Ecology", "Geomicrobiology", 
-                  "Environmental Microbiology", 
-                  "Marine Environmental Microbiology", 
-                  "Plant Microbiology", "Symbiosis", 
-                  "Terrestrial Environmental Microbiology"),
-    "Protein Mechanism" = c("Mechanisms of Resistance", 
-                            "Enzymology and Protein Engineering"),
-    "Evolution & Genetics" = c("Gene Delivery", 
-                               "Genetic Diversity and Evolution",
-                               "Genetics and Molecular Biology", 
-                               "Genome Replication and Regulation of Viral Gene Expression", 
-                               "Evolutionary and Genomic Microbiology"),
-    "Biochemistry" = c("Cellular Biochemistry", "Chemistry; Biosynthesis"),
-    "Modeling" = c("Community Modeling", "Microbial Network Modeling"),
-    "Misc" = c("Brief Case", "Challenging Clinical Case", 
-               "Biographical Feature", "Commentary", "Editorial", "Gem",
-               "Letter to the Editor", "Meeting Presentations", "Meeting Review", 
-               "Photo Quiz", "Point-Counterpoint",
-               "Retraction", "Spotlight", "Author Correction", "Author&apos;s Correction",
-               "Erratum"),
-    "Reviews" = c("Reviews", "Minireview"),
-    "Methods" = c("Methods", "Immunoassays")
-  ))
+  mutate(category = collapse_cat(.$category)) #group categories
 
 cat_cites_plot <- category_cites_data %>% ggplot()+
   geom_boxplot(aes(x=category, y=measure.values))+
--- code/monthly_report/get_asm_usage_overtime.R
+++ code/monthly_report/get_asm_usage_overtime.R
@@ -6,101 +6,18 @@ stats_data <- data %>%
   filter_12_mo(., .$publication.date) %>% #published in last 12 months
   filter(!(measure.names %in% drop.measure.names)) %>% 
   mutate(measure.values.per.month = measure.values/months.published,
-         measure.values.per.k = measure.values/1000) %>% 
-  mutate(category = fct_collapse(category,
-                                 "Antimicrobials" = c("Antibacterial and antifungal agents",
-                                                      "Antiviral agents", "Antiviral Agents",
-                                                      "Pharmacology", "Experimental Therapeutics", 
-                                                      "Biologic Response Modifiers"),
-                                 "Clinical" = c("Clinical Immunology and Vaccines",
-                                                "Clinical Microbiology Best Practices",
-                                                "Clinical Therapeutics",
-                                                "Clinical Veterinary Microbiology", 
-                                                "Fungal and Parasitic Infections",
-                                                "Bacterial Infections",
-                                                "Analytical Procedures",
-                                                "Practical Guidance for Clinical Microbiology (formerly &lt;i&gt;Cumitechs&lt;/i&gt;)"),
-                                 "Virology" = c("Virology", "Virus-Cell Interactions", "Viruses",
-                                                "(-) strand RNA viruses, double-strand RNA viruses",
-                                                "(+) strand RNA viruses, including retroviruses/HIV",
-                                                "Structure and Assembly", "DNA viruses",
-                                                "Transformation and Oncogenesis"),
-                                 "Mycology" = c("Fungi and yeast", "Mycology",
-                                                "Mycobacteriology and Aerobic Actinomycetes"),
-                                 "Parasitology" = c("Parasites", "Parasitology"),
-                                 "Pathogenesis & Immunity" = c("Pathogenesis", 
-                                                               "Pathogenesis and Immunity", 
-                                                               "Molecular Pathogenesis", 
-                                                               "Cellular Microbiology: Pathogen-Host Cell Molecular Interactions", 
-                                                               "Host Response and Inflammation",
-                                                               "Cellular Response to Infection",
-                                                               "Susceptibility"),
-                                 "Vaccine Immunology" = c("Microbial Immunity and Vaccines", 
-                                                          "Vaccines and Antiviral Agents"),
-                                 "Public Health" = c("Epidemiology", "Epidemiology and Surveillance",
-                                                     "Public and Environmental Health Microbiology",
-                                                     "Food Microbiology"),
-                                 "Omics" = c("Metagenomics and bioinformatics", "Molecular Genomics",
-                                             "Microbial Genomics"),
-                                 "Host Microbiomes" = c("Host-Associated Microbial Communities", 
-                                                        "Host-Microbiome Relationships", 
-                                                        "Invertebrate Microbiology", "Microbiome"),
-                                 "Physiology" = c("Microbial Metabolism", 
-                                                  "Mechanisms of Action: Physiological Effects"),
-                                 "Ecology" = c("Microbial Ecology", "Geomicrobiology", 
-                                               "Environmental Microbiology", 
-                                               "Marine Environmental Microbiology", 
-                                               "Plant Microbiology", "Symbiosis", 
-                                               "Terrestrial Environmental Microbiology"),
-                                 "Protein Mechanism" = c("Mechanisms of Resistance", 
-                                                         "Enzymology and Protein Engineering"),
-                                 "Evolution & Genetics" = c("Gene Delivery", 
-                                                            "Genetic Diversity and Evolution",
-                                                            "Genetics and Molecular Biology", 
-                                                            "Genome Replication and Regulation of Viral Gene Expression", 
-                                                            "Evolutionary and Genomic Microbiology"),
-                                 "Biochemistry" = c("Cellular Biochemistry", "Chemistry; Biosynthesis"),
-                                 "Modeling" = c("Community Modeling", "Microbial Network Modeling"),
-                                 "Misc" = c("Brief Case", "Challenging Clinical Case", 
-                                            "Biographical Feature", "Commentary", "Editorial", "Gem",
-                                            "Letter to the Editor", "Meeting Presentations", "Meeting Review", 
-                                            "Photo Quiz", "Point-Counterpoint",
-                                            "Retraction", "Spotlight", "Author Correction", "Author&apos;s Correction",
-                                            "Erratum"),
-                                 "Reviews" = c("Reviews", "Minireview"),
-                                 "Methods" = c("Methods", "Immunoassays")
-  ))
+         measure.values.per.k = measure.values/1000) %>% #divide total measure values by 1000 for plottable values
+  mutate(category = collapse_cat(.$category)) #group categories
 
 #all_stats_data %>% group_by(journal, measure.names) %>% #  summarise(total = sum(measure.values))
 
-#abstract_time <- stats_data %>% 
-#  filter(measure.names == "Abstract") %>% 
-#  ggplot()+
-#  geom_line(aes(x=publication.date, y=measure.values, color = journal))+
-#  facet_wrap(~journal, scales = "free", ncol = 2)+
-#  labs(x = "Publication Date",
-#       y = "Abstract Views",
-#       title = "Abstract Views",
-#       subtitle = "Articles published in last 12 months")+
-#  my_theme_horiz
-#
-#html_time <- stats_data %>% 
-#  filter(measure.names == "HTML") %>% 
-#  ggplot()+
-#  geom_line(aes(x=publication.date, y=measure.values, color = journal))+
-#  facet_wrap(~journal, scales = "free", ncol = 2)+
-#  labs(x = "Publication Date",
-#       y = "HTML Views",
-#       title = "HTML Views",
-#       subtitle = "Articles published in last 12 months")+
-#  my_theme_horiz
-
-#
+#usage stats overtime (abstract, html, pdf) for each journal----
 asm_time <- stats_data %>% 
-  group_by(journal, floor_date(ymd(publication.date), unit = "month"), measure.names) %>% 
-  summarise(total.views = sum(measure.values.per.k)) %>% 
+  group_by(journal, floor_date(ymd(publication.date), #group articles by journal and publication month
+                               unit = "month"), measure.names) %>% 
+  summarise(total.views = sum(measure.values.per.k)) %>% #add values per/1000
   ggplot()+
-  geom_line(aes(x=`floor_date(ymd(publication.date), unit = "month")`, 
+  geom_line(aes(x=`floor_date(ymd(publication.date), unit = "month")`, #plot by year and month
                 y=total.views, color = measure.names, group = measure.names))+
   facet_wrap( ~ journal, scales = "free_y", shrink = TRUE,
               strip.position = "right", ncol = 1)+
@@ -111,9 +28,11 @@ asm_time <- stats_data %>%
        color = "Measures")+
   my_theme_leg
 
+#usage stats overtime grouped according to manuscript categories----
 pdf_cat <- stats_data %>% 
   filter(measure.names == "PDF") %>% 
-  group_by(category, floor_date(ymd(publication.date), unit = "month"), measure.names) %>% 
+  group_by(category, floor_date(ymd(publication.date), 
+                                unit = "month"), measure.names) %>% 
   summarise(total.views = sum(measure.values.per.k)) %>% 
   ggplot()+
   geom_line(aes(x=`floor_date(ymd(publication.date), unit = "month")`, y=total.views, group = category))+
